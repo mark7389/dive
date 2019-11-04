76a81bb0ad13cc5ef9320646903b3ee24d8aaa6c
gnvvbll <gnvvbll@gmail.com>
2019-11-04 17:33:28 -0500

    Installed Bootstrap and other gems

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,11 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+//= require jquery3
+//= require popper
+//= require bootstrap
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
