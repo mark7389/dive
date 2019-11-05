8fbfb54d19eafab9e40431d8466131603cacaf43
gnvvbll <gnvvbll@gmail.com>
2019-11-05 16:57:13 -0500

    bootstrap installed, devise installed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'https://git.heroku.com/dive-genevieve-bell.git' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
