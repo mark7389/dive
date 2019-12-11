d347cc7971d8f9361a60618dc2afa5730309b5af
gnvvbll <gnvvbll@gmail.com>
2019-12-10 22:03:34 -0500

    troubleshooting aws

--- Gemfile
+++ Gemfile
@@ -59,6 +59,8 @@ gem 'popper_js', '~> 1.14.5'
 gem 'simple_form'
 gem 'devise'
 gem 'carrierwave', '~> 2.0'
+gem 'mini_magick'
+gem 'carrierwave-aws'
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,2 +1,11 @@
 CarrierWave.configure do |config|
+    config.storage    = :aws
+    config.aws_bucket = ENV["AWS_BUCKET"]
+    config.aws_acl    = "public-read"
+  
+    config.aws_credentials = {
+        access_key_id:     ENV["AKIAQWHUZWS66WAQNVEM"],
+        secret_access_key: ENV["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"],
+        region:            ENV["us-east-1"]
+    }
 end
