f226c1d2deca1fb5b9541783ccf7183db0a37059
gnvvbll <gnvvbll@gmail.com>
2019-12-17 22:30:14 -0500

    configured Stripe to Dive App

--- Gemfile
+++ Gemfile
@@ -63,6 +63,7 @@ gem 'fog-aws'
 gem 'mini_magick'
 gem 'carrierwave-aws'
 gem 'figaro'
+gem 'stripe'
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,13 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+
     def show
+    end
+
+    def require_authorized_for_current_lesson
+      if current_lesson.user != current_user
+        redirect_to course_path, alert: 'You are not enrolled in this course. Enroll to access this page.'
+      end
     end
 
     private
