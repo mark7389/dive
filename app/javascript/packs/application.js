d78a81c708e974e91a4f518f94a3df0af6e6fdba
gnvvbll <gnvvbll@gmail.com>
2020-01-11 14:45:03 -0500

    redid lessons 30 and 31, added js to environment.js and application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,26 +1,18 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
-//= require jquery3
-//= require popper
-//= require bootstrap
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
-//= require jquery-ui
+import $ from 'jquery';
+require('@rails/ujs').start();
+require('turbolinks').start();
+require('@rails/activestorage').start();
+require('channels');
+global.$ = $;
+global.jQuery = $;
+require('jquery-ui');
+require.context(
+  'file-loader?name=[path][name].[ext]&context=node_modules/jquery-ui-dist!jquery-ui-dist',
+  true,
+  /jquery-ui\.css/
+);
+require.context(
+  'file-loader?name=[path][name].[ext]&context=node_modules/jquery-ui-dist!jquery-ui-dist',
+  true,
+  /jquery-ui\.theme\.css/
+);
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,26 @@
-const { environment } = require('@rails/webpacker')
-
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
+const { environment } = require('@rails/webpacker');
+const webpack = require('webpack');
+// resolve-url-loader must be used before sass-loader
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader',
+  options: {
+    attempts: 1
+  }
+});
+// Add an additional plugin of your choosing : ProvidePlugin
+environment.plugins.prepend(
+  'Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
+    $: 'jquery',
+    JQuery: 'jquery',
+    jquery: 'jquery',
+    'window.Tether': 'tether',
+    Popper: ['popper.js', 'default'] // for Bootstrap 4
   })
-)
-
-module.exports = environment
-
+);
+const aliasConfig = {
+  jquery: 'jquery/src/jquery',
+  'jquery-ui': 'jquery-ui-dist/jquery-ui.js'
+};
+environment.config.set('resolve.alias', aliasConfig);
+module.exports = environment;
