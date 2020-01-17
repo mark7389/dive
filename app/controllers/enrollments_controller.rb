31c1121205d784195549195172116dbccc97eb49
gnvvbll <gnvvbll@gmail.com>
2020-01-16 19:45:20 -0500

    now expecting end of input

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -5,13 +5,13 @@ class EnrollmentsController < ApplicationController
     if current_course.premium?
       # Amount in cents
       @amount = (current_course.cost * 100).to_i
-    end
+
     
       customer = Stripe::Customer.create(
         email: params[:stripeEmail],
         source: params[:stripeToken]
       )
-    end
+ 
 
       charge = Stripe::Charge.create(
         customer: customer.id,
